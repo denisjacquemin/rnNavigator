7f875472b9fe9d4c86cafa263fea9858c3a7cf5d
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-11-16 17:49:42 +0100

    2 Scenes with a transition done

--- index.ios.js
+++ index.ios.js
@@ -7,21 +7,49 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Navigator
+  Navigator,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+  View
 } from 'react-native';
 
 import WelcomeScene from './app/components/WelcomeScene';
 
 export default class rnNavigator extends Component {
   render() {
+    const routes = [
+      {title: 'First Scene', index: 0},
+      {title: 'Second Scene', index: 1},
+    ];
     return (
       <Navigator
-        initialRoute={{ title: 'My Welcome Scene', index: 0 }}
-        renderScene={(route, navigator) => {
-          return <WelcomeScene title={route.title} />
-        }}
+        initialRoute={routes[0]}
+        renderScene={(route, navigator) =>
+          <View style={styles.container}>
+            <TouchableHighlight onPress={() => {
+              if (route.index === 0) {
+                navigator.push(routes[1]);
+              } else {
+                navigator.pop();
+              }
+            }}>
+              <Text>Hello {route.title}!</Text>
+            </TouchableHighlight>
+          </View>
+        }
       />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
 AppRegistry.registerComponent('rnNavigator', () => rnNavigator);
